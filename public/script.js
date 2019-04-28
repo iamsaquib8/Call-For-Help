b9c1dd766d0e782412b74691c00c9a65b19945ff
Saquib Ul Hassan <saquibulhassan6@gmail.com>
2019-04-28 22:26:50 +0530

    changes for heroku

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const url = 'https://safe-lowlands-92086.herokuapp.com/';
+const url = 'https://safe-lowlands-92086.herokuapp.com';
 const register = ()=> {
     const name= document.getElementById('registerName').value;
     const email = document.getElementById('registerEmail').value;
