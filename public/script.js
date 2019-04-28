67f90065577409a54633c5714d213fd702f06202
Saquib Ul Hassan <saquibulhassan6@gmail.com>
2019-04-28 22:26:28 +0530

    changes for heroku

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:8080';
+const url = 'https://safe-lowlands-92086.herokuapp.com/';
 const register = ()=> {
     const name= document.getElementById('registerName').value;
     const email = document.getElementById('registerEmail').value;
